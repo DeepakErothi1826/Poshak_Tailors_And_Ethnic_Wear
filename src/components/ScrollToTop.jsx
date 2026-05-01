7de3001eebfd535d875779c61f70a3d417aed78a
DeepakErothi <you@example.com>
2026-05-01 18:15:58 +0530

    feat: redirect to home page on every refresh

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -1,12 +1,19 @@
 import { useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 export default function ScrollToTop() {
   const { pathname } = useLocation();
+  const navigate = useNavigate();
 
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [pathname]);
+
+  useEffect(() => {
+    if (pathname !== '/') {
+      navigate('/', { replace: true });
+    }
+  }, []);
 
   return null;
 }
