68492cddf40707963ea88dd51d3ae1d3f0caee7a
DeepakErothi <you@example.com>
2026-05-01 18:14:44 +0530

    feat: add page loader on refresh with logo animation

--- src/App.jsx
+++ src/App.jsx
@@ -7,24 +7,27 @@ import About from './pages/About';
 import Contact from './pages/Contact';
 import Fabrics from './pages/Fabrics';
 import ScrollToTop from './components/ScrollToTop';
+import PageLoader from './components/PageLoader';
 
 const basename = window.location.hostname === 'localhost' ? '/' : '/Poshak_Tailors_And_Ethnic_Wear';
 
 function App() {
   return (
-    <Router basename={basename}>
-      <ScrollToTop />
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="products" element={<Products />} />
-          <Route path="product/:id" element={<ProductDetail />} />
-          <Route path="about" element={<About />} />
-          <Route path="contact" element={<Contact />} />
-          <Route path="fabrics" element={<Fabrics />} />
-        </Route>
-      </Routes>
-    </Router>
+    <PageLoader>
+      <Router basename={basename}>
+        <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="products" element={<Products />} />
+            <Route path="product/:id" element={<ProductDetail />} />
+            <Route path="about" element={<About />} />
+            <Route path="contact" element={<Contact />} />
+            <Route path="fabrics" element={<Fabrics />} />
+          </Route>
+        </Routes>
+      </Router>
+    </PageLoader>
   );
 }
 
